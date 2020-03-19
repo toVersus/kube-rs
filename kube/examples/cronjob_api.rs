2f71cd0931008e8c4acd4a9ffd8dd2978ff4c723
clux <sszynrae@gmail.com>
2020-03-19 21:57:55 +0000

    cronjob example - mostly from #191

---
+++ kube/examples/cronjob_api.rs
@@ -0,0 +1,57 @@
+use k8s_openapi::{
+    apimachinery::pkg::apis::meta::v1::ObjectMeta,
+    api::batch::v1beta1::{CronJob, CronJobSpec, JobTemplateSpec},
+    api::batch::v1::JobSpec,
+    api::core::v1::{PodTemplateSpec, PodSpec, Container},
+};
+use kube::{Api, api::PostParams, client::APIClient, config};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    std::env::set_var("RUST_LOG", "info,kube=debug");
+    env_logger::init();
+    let config = config::load_kube_config().await?;
+    let client = APIClient::new(config);
+    let namespace = std::env::var("NAMESPACE").unwrap_or("default".into());
+
+    let api = Api::<CronJob>::namespaced(client, &namespace);
+    let jobname = "blagger".to_string();
+    let cron = "0 * * * *";
+
+    api.create(&PostParams::default(), &CronJob {
+        metadata: Some(ObjectMeta {
+            name: Some(jobname),
+            ..Default::default()
+        }),
+        spec: Some(CronJobSpec {
+            schedule: cron.into(),
+            job_template: JobTemplateSpec {
+                spec: Some(JobSpec {
+                    template: PodTemplateSpec {
+                        spec: Some(PodSpec {
+                            containers: vec![
+                                Container {
+                                    name: "hello".into(),
+                                    image: Some("busybox".into()),
+                                    args: Some(vec!["/bin/sh".into(),
+                                    "-c".into(),
+                                    "date; echo Hello".into()]),
+                                    ..Default::default()
+                                }
+                            ],
+                            restart_policy: Some("OnFailure".into()),
+                            ..Default::default()
+                        }),
+                        metadata: None,
+                    },
+                    ..Default::default()
+                }),
+                metadata: None,
+            },
+            ..Default::default()
+        }),
+        status: None,
+    }).await?;
+
+    Ok(())
+}
